bd3958f16a01a07870da23a086e119e5d9a7f1cb
Paulito Spinach <paulstettnisch@gmail.com>
2018-09-08 17:58:32 -0500

    Got search working and api returning data

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,4 +3,14 @@ class SearchController < ApplicationController
   def index
 
   end
+
+  def new
+    @search_term = params[:q]
+
+    @response = HTTParty.get("http://www.omdbapi.com/?t=#{@search_term}&apikey=61af6132&type=movie")
+
+    puts JSON.pretty_generate(@response)
+  end
+
+
 end
